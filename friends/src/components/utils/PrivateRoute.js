1037e2599f4edb48b56bcf99257722a599efd35b
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-24 08:56:42 +0000

    set up private route file

--- friends/src/components/utils/PrivateRoute.js
+++ friends/src/components/utils/PrivateRoute.js
@@ -1,1 +1,22 @@
-//SPELLING OF AUTHORIZATION: with z
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest}) => {
+
+return (
+
+    <Route
+         {...rest} 
+         render={() => {
+        if(localStorage.getItem('token')) {
+            return <Component {...props} />
+
+        } else {
+            return <Redirect to="/login" />
+        }
+
+    }} />
+)
+}
+
+export default PrivateRoute;
